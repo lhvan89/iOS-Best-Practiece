8276674e2ec65c823ad8817d900387f3674b1611
lhvan89 <lhvan89@gmail.com>
2018-05-02 17:21:30 +0700

    update

---
+++ Tab_Bar/Controllers/DetailViewController.swift
@@ -0,0 +1,39 @@
+//
+//  DetailViewController.swift
+//  Tab_Bar
+//
+//  Created by Van on 5/2/18.
+//  Copyright © 2018 Van. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBAction func btnBack(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
