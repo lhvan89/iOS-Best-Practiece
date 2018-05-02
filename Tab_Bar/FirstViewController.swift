86555fe6adb42cf60d212c7e23b360e0045e8fb1
lhvan89 <lhvan89@gmail.com>
2018-05-02 16:48:39 +0700

    add Tab bar controller

---
+++ Tab_Bar/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Tab_Bar
+//
+//  Created by Van on 5/2/18.
+//  Copyright © 2018 Van. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
